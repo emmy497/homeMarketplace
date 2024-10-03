d9a67599f676d76bfc246c3b6a4b1e23734cfc42
Emmanuel <emmanuelisrael2774@gmail.com>
2024-10-03 09:14:43 +0100

    Fixed listing image not showin

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,6 +3,15 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { db } from "../firebase.config";
+import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 function CreateListing() {
   const [geolocationEnabled, setGeoLocationEnabled] = useState(true);
@@ -83,9 +92,7 @@ function CreateListing() {
     let location;
 
     if (geolocationEnabled) {
-      const response = await fetch(
-        `https://api.distancematrix.ai/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-      );
+      const response = await fetch(`https://api.distancematrix.ai/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
 
       const data = await response.json();
 
@@ -99,19 +106,81 @@ function CreateListing() {
           ? undefined
           : data.result[0]?.formatted_address;
 
-        if(location === undefined || location.includes("undefined") ){
-             setLoading(false)
-             toast.error("Please enter a correct address")
-             return
-        }
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
       location = address;
-      
     }
 
+    //Store images in Firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storagRef = ref(storage, "images/" + filename);
+
+        const uploadTask = uploadBytesResumable(storagRef, image);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is" + progress + " % done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+
+    delete formDataCopy.images;
+    delete formDataCopy.address;
+    location && (formDataCopy.location = location);
+
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+
     setLoading(false);
+    toast.success("Listing saved");
+    navigate(`/catrgory/${formDataCopy.type}/${docRef.id}`);
   };
 
   const onMutate = (e) => {
@@ -206,8 +275,8 @@ function CreateListing() {
               <input
                 type="number"
                 className="formInputSmall"
-                id="bedrooms"
-                value={bedrooms}
+                id="bathrooms"
+                value={bathrooms}
                 onChange={onMutate}
                 min="1"
                 max="50"
